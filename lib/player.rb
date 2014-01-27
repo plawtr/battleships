e786117d81409a90e730fff914faf9a7e41f45d1
biwek <biweks@gmail.com>
2014-01-27 14:59:28 +0000

    added starting point method and tests

--- lib/player.rb
+++ lib/player.rb
@@ -4,27 +4,43 @@ class Player
   # You will have to create a board in there and randomize the
   # positioning of your ships on the board.
   
+  SHIPS = [5, 4, 3, 2, 2, 1, 1]
+
   attr_reader :name
 
-  def initialize(name, board = [])
+  def initialize(name)
     @name = name
-    @board ||= board
+    @board = Board.new(self)
     # self.board = board.fetch(:board, DEFAULT)
 
   end
 
-  def board(board)
-    @board = board.rows
+  def board
+    @board
   end
 
-  def board_randomize
-    ships = [5, 4, 3, 2, 2, 1, 1]
-    positions = []
+  def board_randomize(board)
+    # positions = []
     i = 0
-    while i < ships[i]
-      positions << @board[rand(0..9)].sample(ships[i])
+    while i < SHIPS[i]
+      # positions << @board[rand(0..9)].sample(ships[i])  # @board[rand(0..9)].sample(ships[i]).map { |x| x.replace("s")}
+      s = starting_point
+
+
+
+
       i += 1
     end
+  end
+
+  def starting_point
+    board.field.keys[rand(0..99)]
+  end
+
+  def hit_wall?
+
+    
+
   end
 
   
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,4 +1,5 @@
 require_relative "../lib/player"
+require_relative "../lib/board"
 
 describe Player do
 
@@ -9,12 +10,12 @@ describe Player do
 		expect(player.name).to eq("John")
 	end
 
-	it "should return a board" do  
-		board = double :board
-		player = Player.new("John")
-		expect(board).to receive(:rows)
-		player.board(board)
-	end
+	# it "should return a board" do  
+	# 	board = double :board
+	# 	player = Player.new("John")
+	# 	expect(board).to receive(:create_field)
+	# 	player.board(board)
+	# end
 
 	# it "should randomize the positioning of ships" do  
 	# 	# board = double :board
@@ -27,11 +28,20 @@ describe Player do
 	# 	expect(board).to have_ships
 	# end
 
+	it "should choose random starting position of ships" do  
+		player = Player.new("John")
+		board = Board.new(player)
+		expect((board.field.keys).include?(player.starting_point)).to be_true
+	end
+
+
+
+
 	it "should shoot at the opponents board" do  
 		board = double :board
 		player = Player.new("John")
 		expect(board).to receive(:register_shot)
-		player.shoot("A4", board)
+		player.shoot("a4", board)
 	end
 
 	# it "should have ships floating" do 
