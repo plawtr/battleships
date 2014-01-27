6ecc7a590496d0978fc1ec3e4a9d878f2ac9ac3c
biwek <biweks@gmail.com>
2014-01-27 13:00:00 +0000

    added test and updated shoot method

--- lib/player.rb
+++ lib/player.rb
@@ -6,21 +6,37 @@ class Player
   
   attr_reader :name
 
-  def initialize(name, board = {})
+  def initialize(name, board = [])
     @name = name
     @board ||= board
+    # self.board = board.fetch(:board, DEFAULT)
+
   end
 
   def board(board)
     @board = board.rows
   end
+
+  def board_randomize
+    ships = [5, 4, 3, 2, 2, 1, 1]
+    positions = []
+    i = 0
+    while i < ships[i]
+      positions << @board[rand(0..9)].sample(ships[i])
+      i += 1
+    end
+  end
+
   
   # Tells us if there are still ships that have not been hit
   # on her/his board.
   def has_ships_still_floating?
+
   end
   
   # Shoots at the opponents board.
   def shoot(at_coordinates, opponent_board)
+    opponent_board.register_shot(at_coordinates)
   end
+
 end
